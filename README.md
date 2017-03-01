1b1516522af5fa3b0a986591e88ab213aafed3c9
Enrico Cavallin <enricocavallin@MacBook-Air-di-Enrico.local>
2017-03-01 23:53:09 +0100

    Node server serving some stati files (html, js, css) and an incomplete manifest

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # application-cache-test
+```
+> cd <sample_dir>       //example: cd 1-helloworld
+> npm install           //to install packages needed
+> npm start & npm test  //to run the sample server and related test
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+const port = 3001;
+
+
+app.get("/index.html", (req, res) => {
+    console.log("serving index.html");
+    res.setHeader("Cache-Control", "private, max-age=10");
+    res.sendFile(__dirname + "/assets/index.html");
+});
+app.get("/style.css", (req, res) => {
+    console.log("serving style.css");
+    res.setHeader("Cache-Control", "private, no-store");
+    res.sendFile(__dirname + "/assets/style.css");
+});
+app.get("/debug.js", (req, res) => {
+    console.log("serving debug.js");
+    res.setHeader("Cache-Control", "private, max-age=1000");
+    res.sendFile(__dirname + "/assets/debug.js");
+});
+app.get("/offline.appcache", (req, res) => {
+    console.log("serving offline.appcache");
+    res.setHeader("Cache-Control", "private, no-cache");
+    res.sendFile(__dirname + "/assets/offline.appcache");
+});
+
+
+// app.use(express.static(path.join(__dirname, "assets")));
+
+app.listen(port, () => console.log("Serving static with express on :" + port));
